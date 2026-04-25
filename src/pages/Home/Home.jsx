e58481267ad55a6582603309ae7c4627d5c41c93
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-25 13:33:25 +0530

    Removed code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import Welcome from './sections/Welcome';
-import Features from './sections/Features';
-import Statistics from './sections/Statistics';
-import Testimonials from './sections/Testimonials';
-import HowItWorks from './sections/HowItWorks';
+import { Features, HowItWorks, Statistics, Testimonials, Welcome } from './sections';
 
 export default function Home() {
     return (
         <>
-            <div>
-                <Welcome />
-                <Features />
-                <HowItWorks />
-                <Statistics />
-                <Testimonials />
-            </div>
+            <Welcome />
+            <Features />
+            <HowItWorks />
+            <Statistics />
+            <Testimonials />
         </>
     );
 }
