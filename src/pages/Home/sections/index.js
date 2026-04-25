afe6e2979047ff635cb2f28e6c6db3ae05bce395
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-25 13:32:22 +0530

    Created a file where stored all sections files for clean & structured code

---
+++ src/pages/Home/sections/index.js
@@ -0,0 +1,5 @@
+export { default as Welcome } from './Welcome';
+export { default as Features } from './Features';
+export { default as HowItWorks } from './HowItWorks';
+export { default as Statistics } from './Statistics';
+export { default as Testimonials } from './Testimonials';
